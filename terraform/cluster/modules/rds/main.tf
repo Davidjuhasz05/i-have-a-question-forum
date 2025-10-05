c39ad24f8d343cde816d2793c1d6ceb7358f5761
Davidjuhasz05 <juhasz.david.2005@gmail.com>
2025-10-05 19:01:58 +0200

    fix(rds): Change username and add custom id

--- terraform/cluster/modules/rds/main.tf
+++ terraform/cluster/modules/rds/main.tf
@@ -24,12 +24,13 @@ resource "aws_security_group" "rds_sg" {
 }
 
 resource "aws_db_instance" "rds" {
+  identifier           = "ihaq-rds-database"
   allocated_storage    = 20
   engine               = "postgres"
   engine_version       = "17.4"
   instance_class       = "db.t3.micro"
   db_name              = "ihaq"
-  username             = "ihaq-user"
+  username             = "ihaquser"
   password             = "ihaq-admin"
   db_subnet_group_name = aws_db_subnet_group.rds_subnets.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
